d0ca8355cdbe64233ac4758e31c72e6fbb874f06
thiomajid <82466066+ATOM36@users.noreply.github.com>
2023-10-28 17:33:23 +0100

    Changed main class name

--- consolequest/Program.java
+++ consolequest/Program.java
@@ -5,7 +5,7 @@ import consolequest.tp1.GameEngine;
 /**
  * The main class of the game.
  */
-public class ConsoleQuest {
+public class Program {
     public static void main(String[] args) {
         GameEngine game = new GameEngine();
         game.run();
